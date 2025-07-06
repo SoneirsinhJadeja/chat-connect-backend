f525f4e69c2d6f39cf192e2905dfbbc7281432d1
sonei <soneirsinh81205@gmail.com>
2025-07-06 23:34:55 +0530

    Use MONGO_URI from environment instead of localhost

--- db.js
+++ db.js
@@ -1,20 +1,38 @@
+// const mongoose = require('mongoose');
+
+// const mongooseURL = 'mongodb://localhost:27017/chatConnect';
+
+// mongoose.connect(mongooseURL); // No extra options needed now
+
+// const db = mongoose.connection;
+
+// db.on('connected', () => {
+//     console.log('Connected to MongoDB Server');
+// });
+// db.on('error', (err) => {
+//     console.log('Error connecting to MongoDB Server:', err);
+// });
+// db.on('disconnected', () => {
+//     console.log('MongoDB Disconnected');
+// });
+
+// module.exports = db;
+ 
+
 const mongoose = require('mongoose');
 
-const mongooseURL = 'mongodb://localhost:27017/chatConnect';
+// use your Render environment variable
+const mongooseURL = process.env.MONGO_URI;
 
-mongoose.connect(mongooseURL); // No extra options needed now
-
-const db = mongoose.connection;
-
-db.on('connected', () => {
-    console.log('Connected to MongoDB Server');
-});
-db.on('error', (err) => {
-    console.log('Error connecting to MongoDB Server:', err);
-});
-db.on('disconnected', () => {
-    console.log('MongoDB Disconnected');
+mongoose.connect(mongooseURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('✅ Connected to MongoDB Atlas');
+})
+.catch((err) => {
+    console.error('❌ MongoDB Atlas connection error:', err.message);
 });
 
-module.exports = db;
- 
+module.exports = mongoose.connection;
