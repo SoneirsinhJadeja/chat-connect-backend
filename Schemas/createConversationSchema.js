d68e993f8502af7447356361d4b622a3dc56237b
sonei <soneirsinh81205@gmail.com>
2025-07-27 17:55:03 +0530

    change schema in create conversation so now users can also rename the users name if needed !!

--- Schemas/createConversationSchema.js
+++ Schemas/createConversationSchema.js
@@ -1,20 +1,14 @@
 const mongoose = require('mongoose');
 
 const participantSchema = new mongoose.Schema({
-  email: String,
+  email: { type: String, required: true },
   name: String,
-  dp: {
-    type: String,
-    default: null
-  },
+  dp: { type: String, default: null },
   nickname: String
 });
 
 const conversationSchema = new mongoose.Schema({
-  chatOwner: {
-    type: String,
-    required: true
-  },
+  chatOwner: { type: String, required: true },
   participants: [participantSchema],
   createdAt: String
 });
